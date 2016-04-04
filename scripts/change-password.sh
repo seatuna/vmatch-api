212429002700121180f46fe5110d2e914d648b7a
Celena T <chibiynm@gmail.com>
2016-04-03 23:30:15 -0400

    Edit user curl scripts

--- scripts/change-password.sh
+++ scripts/change-password.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-curl --include --request PATCH http://localhost:3000/change-password/$ID \
-  --header "Authorization: Token token=$TOKEN" \
+curl --include --request PATCH http://localhost:3000/change-password/1 \
+  --header "Authorization: Token token=BAhJIiU3OTlmNGFiMDlmZTYxYjBhMjA5ODE5YjRmZDMzMjhkMAY6BkVG--ed3d2631294e9f654543122341b4cd02cf474e17" \
   --header "Content-Type: application/json" \
   --data '{
     "passwords": {
-      "old": "an example password",
-      "new": "super sekrit"
+      "old": "stillfishy",
+      "new": "fishy"
     }
   }'
--- scripts/sign-out.sh
+++ scripts/sign-out.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl --include --request DELETE http://localhost:3000/sign-out/$ID \
-  --header "Authorization: Token token=$TOKEN"
+curl --include --request DELETE http://localhost:3000/sign-out/1 \
+  --header "Authorization: Token token=BAhJIiVjODNiMGFiODY5ZjdjOTJmMjc4ZWIyNjYyYjk0NjAyOQY6BkVG--3128e4d2a014c8b31694ca764905db5774c5d7ed"
