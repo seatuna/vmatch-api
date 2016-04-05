5abe2b2e3f9e1045569ec4d6c922aa3c5f792644
Celena T <chibiynm@gmail.com>
2016-04-05 16:42:56 -0400

    Remove extra comments

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,48 +1,34 @@
 class PlayersController < OpenReadController
   before_action :set_player, only: [:show, :update, :destroy]
 
-  # GET /players
-  # GET /players.json
   def index
     @players = Player.all
     render json: @players
   end
 
-  # GET /players/1
-  # GET /players/1.json
   def show
     render json: @player
   end
 
-  # POST /players
-  # POST /players.json
   def create
-
     if current_user
       @player = Player.new(player_params)
-      selected_character1 = Character.find(params[:selected_character1])
-      selected_opponent1 = Character.find(params[:selected_opponent1])
-      selected_character2 = Character.find(params[:selected_character2])
-      selected_opponent2 = Character.find(params[:selected_opponent2])
-      selected_character3 = Character.find(params[:selected_character3])
-      selected_opponent3 = Character.find(params[:selected_opponent3])
+      selected_character = Character.find(params[:selected_character])
+      selected_opponent = Character.find(params[:selected_opponent])
 
-      @player.characters << selected_character1 << selected_character2 << selected_character3
-      @player.opponents << selected_opponent1 << selected_opponent2 << selected_opponent3
+      @player.characters << selected_character
+      @player.opponents << selected_opponent
 
       if @player.save
         render json: @player, status: :created, location: @player
       else
         render json: @player.errors, status: :unprocessable_entity
       end
-
     else
       head :bad_request
     end
   end
 
-  # PATCH/PUT /players/1
-  # PATCH/PUT /players/1.json
   def update
     @player = Player.find(params[:id])
 
@@ -53,8 +39,6 @@ class PlayersController < OpenReadController
     end
   end
 
-  # DELETE /players/1
-  # DELETE /players/1.json
   def destroy
     @player.destroy
 
