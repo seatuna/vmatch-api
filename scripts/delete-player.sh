c813b8f0c177a0a26f84efcf4d75f2742c6c13b1
Celena T <chibiynm@gmail.com>
2016-04-05 16:13:11 -0400

    Create working delete curl script

---
+++ scripts/delete-player.sh
@@ -0,0 +1,2 @@
+curl --include --request DELETE http://localhost:3000/players/7 \
+  --header "Authorization: Token token=BAhJIiU3OGYyZTBiMzkwMjdlNjljMGYzZDIyMWUwNzhhZTg0NQY6BkVG--3d4993bba6241c57d0e314baa66186a90f62d161"
